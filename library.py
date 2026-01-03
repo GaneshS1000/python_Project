cdeb05c5dc02846952feb134c18a37a6282c9d36
GaneshS1000 <ganeshsubramaniannew@gmail.com>
2026-01-03 13:52:16 +0530

    Update library.py

--- library.py
+++ library.py
@@ -1,12 +1,12 @@
 import requests
 
-addBook = requests.post("http://216.10.245.166/Library/Addbook.php",json={"name":"Fire And Ice","isbn":"UK151","aisle":"128","author":"George Martin"})
+addBook = requests.post("http://216.10.245.166/Library/Addbook.php",json={"name":"Game of Thrones","isbn":"UK151","aisle":"129","author":"George Martin"})
 print("Add Book Status Code:",addBook.status_code)
 print(addBook.text)
 print(addBook.headers)
 print(addBook.json())
-getBookDetails = requests.get("http://216.10.245.166/Library/GetBook.php?ID=UK151128")
+getBookDetails = requests.get("http://216.10.245.166/Library/GetBook.php?ID=UK151129")
 print(getBookDetails.json())
-deleteBook = requests.post("http://216.10.245.166/Library/DeleteBook.php",json={"ID":"UK151128"})
+deleteBook = requests.post("http://216.10.245.166/Library/DeleteBook.php",json={"ID":"UK151129"})
 print("Delete Book Status Code:",deleteBook.status_code)
 print(deleteBook.json())
